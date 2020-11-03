94b65222697e1f105bfd3c234f720fdc5657cfea
seido <seid783@gmail.com>
2020-11-02 22:46:53 -0800

    not sure what to commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cookie-stand
+
+- contribiution
+
+i was stuck for more than 2 hours but with the help of Skyler (TA)
+and the rest me and Wondweson my classmate try to figure it out together
+and got stuck again and waiting for a TA again because i cant go further with the code
--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var operationHours=['6am','7am','8am','9am','10am','11am','12am','1pm','2pm','3p','4pm','5pm','6pm','7pm'];
-var seattleStore= document.getElementById('seattle');
+var seattleParent= document.getElementById('seattle');
 var seattleshop={
     minCustomerPerhour:23,
     maxCustomerPerhour:65,
@@ -20,5 +20,10 @@ var seattleshop={
     var cookies = this.getCustomerPerhour() * this.avgCookiesSale
     this.cookiesPerhour.push(Math.round(cookies));
 }
+},
+
+
+ 
 }
-}
+
+
